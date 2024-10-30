a634cb6124b40c8d355b72ea57df2892bfac457c
andre <andreaferraboli2004@gmail.com>
2024-10-30 18:07:14 +0100

    change icons in add meal screen

--- lib/routes/add_meal_screen.dart
+++ lib/routes/add_meal_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hugeicons/hugeicons.dart';
 import 'package:tracker/routes/product_selection_screen.dart';
 
 import '../models/meal_type.dart';
@@ -11,41 +12,33 @@ class AddMealScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
 
-    Color colazioneColor = Theme.of(context).brightness == Brightness.dark
-        ? Colors.deepOrange
-        : Colors.orange;
-    Color pranzoColor = Theme.of(context).brightness == Brightness.dark
-        ? Colors.lightGreen
-        : Colors.green;
-    Color merendaColor = Theme.of(context).brightness == Brightness.dark
-        ? Colors.deepPurple
-        : Colors.purple;
-    Color cenaColor = Theme.of(context).brightness == Brightness.dark
-        ? Colors.lightBlue
-        : Colors.blue;
+    Color colazioneColor = Color.fromRGBO(30, 144, 255, 1.0);
+    Color pranzoColor =  Color.fromRGBO(243, 238, 13, 1.0);
+    Color merendaColor =  Color.fromRGBO(236, 124, 38, 1.0);
+    Color cenaColor = Color.fromRGBO(0, 194, 0, 1.0);
     final List<MealType> mealTypes = [
       MealType(
         name: 'Breakfast',
-        icon: Icons.breakfast_dining,
+        icon: HugeIcons.strokeRoundedCoffee02,
         color: colazioneColor,
       ),
       const MealType.empty(),
       const MealType.empty(),
       MealType(
         name: 'Lunch',
-        icon: Icons.lunch_dining,
+        icon: Icons.fastfood_outlined,
         color: pranzoColor,
       ),
       MealType(
         name: 'Snack',
-        icon: Icons.cookie,
+        icon: HugeIcons.strokeRoundedFrenchFries02,
         color: merendaColor,
       ),
       const MealType.empty(),
       const MealType.empty(),
       MealType(
         name: 'Dinner',
-        icon: Icons.dinner_dining,
+        icon: Icons.dinner_dining_outlined,
         color: cenaColor,
       ),
     ];
