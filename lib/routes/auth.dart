ea8dba8fcae5ac98e886ecd6d6d78ee85114808c
andrea.ferraboli <andrew.ferro04@gmail.com>
2024-11-08 14:03:25 +0100

    fix auth

--- lib/routes/auth.dart
+++ lib/routes/auth.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart'; // Import di Riverpod
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tracker/models/product.dart';
 import 'package:tracker/providers/category_provider.dart';
 import 'package:tracker/providers/products_provider.dart';
@@ -12,7 +12,6 @@ import '../models/expense.dart';
 import '../providers/expenses_provider.dart';
 
 class AuthPage extends ConsumerStatefulWidget {
-  // ConsumerStatefulWidget per Riverpod
   const AuthPage({super.key});
 
   @override
@@ -20,7 +19,6 @@ class AuthPage extends ConsumerStatefulWidget {
 }
 
 class _AuthPageState extends ConsumerState<AuthPage> {
-  // ConsumerState per Riverpod
   bool isLogin = true;
   final _formKey = GlobalKey<FormState>();
   final _emailController = TextEditingController();
@@ -28,13 +26,15 @@ class _AuthPageState extends ConsumerState<AuthPage> {
   final _confirmPasswordController = TextEditingController();
   final _usernameController = TextEditingController();
 
-  // Funzione per autenticare l'utente
   Future<void> _authenticateUser() async {
     try {
       if (isLogin) {
-        loadUserData(); // Caricamento dati utente
+        await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: _emailController.text.trim(),
+          password: _passwordController.text.trim(),
+        );
+        loadUserData();
       } else {
-        // Verifica se le password corrispondono
         if (_passwordController.text.trim() !=
             _confirmPasswordController.text.trim()) {
           ScaffoldMessenger.of(context).showSnackBar(
@@ -45,37 +45,41 @@ class _AuthPageState extends ConsumerState<AuthPage> {
           return;
         }
 
-        // Creazione documenti utente nelle collezioni Firestore
-        DocumentReference userDocRef = FirebaseFirestore.instance
-            .collection('products')
-            .doc(FirebaseAuth.instance.currentUser!.uid);
-        await userDocRef.set({
-          "products": [],
-        });
+        final userCredential =
+            await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: _emailController.text.trim(),
+          password: _passwordController.text.trim(),
+        );
 
-        userDocRef = FirebaseFirestore.instance
-            .collection('expenses')
-            .doc(FirebaseAuth.instance.currentUser!.uid);
-        await userDocRef.set({
-          "expenses": [],
-        });
+        final userDocRef = FirebaseFirestore.instance
+            .collection('users')
+            .doc(userCredential.user!.uid);
 
-        userDocRef = FirebaseFirestore.instance
-            .collection('meals')
-            .doc(FirebaseAuth.instance.currentUser!.uid);
+        // Creazione documento principale dell'utente
         await userDocRef.set({
-          "meals": [],
-        });
-
-        userDocRef = FirebaseFirestore.instance
-            .collection('categories')
-            .doc(FirebaseAuth.instance.currentUser!.uid);
-        await userDocRef.set({
+          "username": _usernameController.text.trim(),
           "categories": await CategoryServices.getAndLoadCategoriesData(),
         });
+
+// Creazione documento per products
+        await FirebaseFirestore.instance
+            .collection('products')
+            .doc(userCredential.user!.uid)
+            .set({"products": []});
+
+// Creazione documento per expenses
+        await FirebaseFirestore.instance
+            .collection('expenses')
+            .doc(userCredential.user!.uid)
+            .set({"expenses": []});
+
+// Creazione documento per meals
+        await FirebaseFirestore.instance
+            .collection('meals')
+            .doc(userCredential.user!.uid)
+            .set({"meals": []});
       }
 
-      // Mostra messaggio di successo
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
           content: Text(
@@ -86,9 +90,6 @@ class _AuthPageState extends ConsumerState<AuthPage> {
         ),
       );
     } on FirebaseAuthException catch (e) {
-      print("errore${e.message}");
-
-      // Gestisce gli errori di autenticazione
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
           content: Text('${AppLocalizations.of(context)!.error}: ${e.message}'),
@@ -99,42 +100,38 @@ class _AuthPageState extends ConsumerState<AuthPage> {
 
   Future<void> loadUserData() async {
     final userId = FirebaseAuth.instance.currentUser!.uid;
+
     final productsDocRef =
         FirebaseFirestore.instance.collection('products').doc(userId);
-
     final productsDoc = await productsDocRef.get();
-    final products = (productsDoc.data()!['products'] as List)
-        .map((product) => Product.fromJson(product))
-        .toList();
+    final products = (productsDoc.data()?['products'] as List?)
+            ?.map((product) => Product.fromJson(product))
+            .toList() ??
+        [];
+
     final categoriesDocRef =
         FirebaseFirestore.instance.collection('categories').doc(userId);
     final categoriesDoc = await categoriesDocRef.get();
-    final categories = (categoriesDoc.data()!['categories'] as List)
-        .map((category) => Category.fromJson(category))
-        .toList();
+    final categories = (categoriesDoc.data()?['categories'] as List?)
+            ?.map((category) => Category.fromJson(category))
+            .toList() ??
+        [];
+
     final expensesDocRef =
         FirebaseFirestore.instance.collection('expenses').doc(userId);
     final expensesDoc = await expensesDocRef.get();
-    final expenses = (expensesDoc.data()!['expenses'] as List)
-        .map((expense) => Expense.fromJson(expense))
-        .toList();
-    // final mealsDocRef = FirebaseFirestore.instance.collection('meals').doc(userId);
-    // final mealsDoc = await mealsDocRef.get();
-    // final meals = (mealsDoc.data()!['meals'] as List)
-    //     .map((meal) => Meal.fromJson(meal))
-    //     .toList();
+    final expenses = (expensesDoc.data()?['expenses'] as List?)
+            ?.map((expense) => Expense.fromJson(expense))
+            .toList() ??
+        [];
 
-    ref
-        .read(productsProvider.notifier)
-        .loadProducts(products); // ref.read per Riverpod
+    ref.read(productsProvider.notifier).loadProducts(products);
     ref.read(categoriesProvider.notifier).loadCategories(categories);
     ref.read(expensesProvider.notifier).loadExpenses(expenses);
-    // ref.read(mealsProvider.notifier).state = meals;
   }
 
   @override
   void dispose() {
-    // Rilascia i controller quando non sono più necessari
     _emailController.dispose();
     _passwordController.dispose();
     _confirmPasswordController.dispose();
@@ -176,7 +173,6 @@ class _AuthPageState extends ConsumerState<AuthPage> {
                 decoration: InputDecoration(
                   labelText: AppLocalizations.of(context)!.email,
                 ),
-                textCapitalization: TextCapitalization.none,
                 keyboardType: TextInputType.emailAddress,
                 validator: (value) {
                   if (value == null || value.isEmpty) {
